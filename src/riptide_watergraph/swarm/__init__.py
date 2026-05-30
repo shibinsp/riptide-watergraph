05587a7421e0f22fd1509cc6420a36a62568cf67
shibinsp <shibinsp43@gmail.com>
2026-05-30 21:31:48 +0530

    chore(swarm): export LLMSwarmComposer

--- src/riptide_watergraph/swarm/__init__.py
+++ src/riptide_watergraph/swarm/__init__.py
@@ -1,6 +1,7 @@
 """Swarm composer implementations."""
 
 from .heuristic_composer import HeuristicSwarmComposer
+from .llm_composer import LLMSwarmComposer
 from .static_composer import SingleAgentComposer
 
-__all__ = ["SingleAgentComposer", "HeuristicSwarmComposer"]
+__all__ = ["SingleAgentComposer", "HeuristicSwarmComposer", "LLMSwarmComposer"]

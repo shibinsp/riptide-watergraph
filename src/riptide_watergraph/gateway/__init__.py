f625f5d7f951e45b8fed9161e85882edf950dfec
shibinsp <shibinsp43@gmail.com>
2026-05-30 21:06:41 +0530

    chore(gateway): export ResilientGateway

--- src/riptide_watergraph/gateway/__init__.py
+++ src/riptide_watergraph/gateway/__init__.py
@@ -2,5 +2,6 @@
 
 from .demo_gateway import DemoGateway
 from .litellm_gateway import LiteLLMGateway
+from .resilient import ResilientGateway
 
-__all__ = ["LiteLLMGateway", "DemoGateway"]
+__all__ = ["LiteLLMGateway", "DemoGateway", "ResilientGateway"]

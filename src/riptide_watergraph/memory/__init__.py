34296c376be3dfbb92e89fb7517571aa86ba7c15
shibinsp <shibinsp43@gmail.com>
2026-05-30 21:19:32 +0530

    chore(memory): export embeddings and reranker

--- src/riptide_watergraph/memory/__init__.py
+++ src/riptide_watergraph/memory/__init__.py
@@ -1,15 +1,20 @@
 """Memory implementations: in-process + persistent, with hybrid ranking + reflection."""
 
+from .embedding import HashingEmbedding, LiteLLMEmbedding
 from .inmemory import InMemoryMemory
 from .jsonfile import JsonFileMemory
 from .ranking import bm25_score, hybrid_rank, rrf_fuse, tokenize
 from .reflection import LLMReflector
+from .rerank import LexicalOverlapReranker
 from .types import MemoryType, lesson_record
 
 __all__ = [
     "InMemoryMemory",
     "JsonFileMemory",
     "LLMReflector",
+    "HashingEmbedding",
+    "LiteLLMEmbedding",
+    "LexicalOverlapReranker",
     "MemoryType",
     "lesson_record",
     "hybrid_rank",

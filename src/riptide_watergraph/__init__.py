4c0bdf097bc22c863048bb0572ff294fcad8ecee
shibinsp <shibinsp43@gmail.com>
2026-06-02 15:02:50 +0530

    build: bump __version__ to 0.3.0

--- src/riptide_watergraph/__init__.py
+++ src/riptide_watergraph/__init__.py
@@ -8,7 +8,7 @@ Public surface (Stage 1):
 
 from __future__ import annotations
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from .gateway import DemoGateway, LiteLLMGateway, ResilientGateway
 from .graph import build_graph

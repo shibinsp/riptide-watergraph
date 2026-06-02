1e6fc930c9bdaebfd0ee64abf5e19bf052657fbc
shibinsp <shibinsp43@gmail.com>
2026-06-02 21:27:46 +0530

    feat(swarm): export StaticPlanComposer

--- src/riptide_watergraph/swarm/__init__.py
+++ src/riptide_watergraph/swarm/__init__.py
@@ -2,6 +2,12 @@
 
 from .heuristic_composer import HeuristicSwarmComposer
 from .llm_composer import LLMSwarmComposer
+from .plan_composer import StaticPlanComposer
 from .static_composer import SingleAgentComposer
 
-__all__ = ["SingleAgentComposer", "HeuristicSwarmComposer", "LLMSwarmComposer"]
+__all__ = [
+    "SingleAgentComposer",
+    "HeuristicSwarmComposer",
+    "LLMSwarmComposer",
+    "StaticPlanComposer",
+]
